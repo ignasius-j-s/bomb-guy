b30fe3c3d6b5b18956e6a51cc93a5538386c17a1
Ircadius <ignis19@protonmail.com>
2023-11-24 08:04:59 +0700

    code cleanup

--- scripts/HealthBar.gd
+++ scripts/HealthBar.gd
@@ -1,7 +1,6 @@
 extends Sprite2D
 
 @onready var player: BombGuy = get_node("../BombGuy")
-@onready var hearts: Array[Sprite2D] = [$Heart1, $Heart2, $Heart3]
 
 func _ready() -> void:
 	position = Vector2(20, 20)
@@ -11,7 +10,7 @@ func _ready() -> void:
 	update_health_bar()
 
 func update_health_bar():
-	hearts[0].visible = player.lives >= 1
-	hearts[1].visible = player.lives >= 2
-	hearts[2].visible = player.lives >= 3
+	$Heart1.visible = player.lives >= 1
+	$Heart2.visible = player.lives >= 2
+	$Heart3.visible = player.lives >= 3
 
