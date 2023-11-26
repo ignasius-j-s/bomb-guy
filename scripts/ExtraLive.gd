10298d21da1ce000d402f2e101f216448267fd5a
Ircadius <ignis19@protonmail.com>
2023-11-26 19:22:24 +0700

    use tween when extra live collected

--- scripts/ExtraLive.gd
+++ scripts/ExtraLive.gd
@@ -6,7 +6,13 @@ func _ready() -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.lives < 3:
+		collected_tween()
 		play("collected")
 		body.get_extra_live.emit()
 		await animation_finished
 		queue_free()
+
+
+func collected_tween():
+	var tween = get_tree().create_tween().bind_node(self)
+	tween.tween_property(self, "scale", Vector2(2,2), 0.4)
